237b2b74c8b085c72d0bed6fa5a5c12179a26d67
Neil Flatley <33094128+nflatley-zengenti@users.noreply.github.com>
2026-08-07 12:39:59 +0000

    contensis-cli-spike: update 1.6.0 bottle.

--- Formula/contensis-cli-spike.rb
+++ Formula/contensis-cli-spike.rb
@@ -29,9 +29,10 @@ class ContensisCliSpike < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/contensis/cli"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "24ac2e9f68294cbcfd0ca4c1ae7aa1c1a63583a6ab06a3f2e0ac190908b7402a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6916ecd4fdf2850299e3bc56d24c5c0d5dc3fdaff41b0459a16d7b889910fcda"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c2e8601530b6659bc04c791efba6f38558bbb5415a0b92e93cc2df66ebe7c1ae"
+    rebuild 1
+    sha256               arm64_sequoia: "5a77dcb85282717d601a987cbfca3a541a013e46a00234e4b7aaeba7d7012e48"
+    sha256 cellar: :any, arm64_linux:   "b46b95671e215619227a8aff281fb66987008878f53740bea711d1a586564810"
+    sha256 cellar: :any, x86_64_linux:  "3774fdaeef075be30b7fa2602c53ccefb7057237ea0f82a6725a2292e0d9cf56"
   end
 
   depends_on "node"
